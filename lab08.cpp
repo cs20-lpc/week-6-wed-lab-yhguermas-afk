32b1907248b2c7219d44ba89309c8d825fb790a0
Yanni Guermas <yanniguermas@Yannis-MacBook-Air.local>
2026-03-02 23:54:38 -0800

    lab08

--- lab08.cpp
+++ lab08.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 /*******************************************************************************
- * Function prototypes
+ * Tail-recursive helper function prototypes
+*******************************************************************************/
+
+unsigned factHelper(unsigned, unsigned);
+unsigned fibHelper(unsigned, unsigned, unsigned);
+unsigned multHelper(unsigned, unsigned, unsigned);
+unsigned powerHelper(unsigned, unsigned, unsigned);
+unsigned productHelper(unsigned, unsigned, unsigned);
+
+/*******************************************************************************
+ * Original function prototypes (unchanged)
 *******************************************************************************/
 
 unsigned fact(unsigned);
@@ -13,94 +23,77 @@ unsigned power(unsigned, unsigned);
 unsigned product(unsigned, unsigned);
 
 /*******************************************************************************
- * Description:
- * Starting point of the program. Calls various recursive functions that can be
- * optimized using tail recursion.
- * 
- * Input:
- * N/A
- *
- * Output:
- * An integer to signal to the OS the exit code.
+ * MAIN
 *******************************************************************************/
 
 int main() {
-    // try a ridiculous case that won't work without tail recursion
     cout << "6 * 123000 = " << mult(6, 123000) << endl;
-    
-    // these functions can't demonstrate the usefulness of tail recursion
-    // due to data type overflow, but still, good practice
     cout << "3 ^ 10 = " << power(3, 10) << endl;
     cout << "8 * 9 * ... * 15 = " << product(8, 15) << endl;
     cout << "10! = " << fact(10) << endl;
-
-    // without tail recursion, this next call is going to take a while
     cout << "50th Fibonacci number is " << fib(50) << endl;
-
-    // terminate
     return 0;
 }
 
 /*******************************************************************************
- * Function definitions below are NOT tail recursive!
- * TODO: make them tail recursive :)
+ * Tail Recursive Implementations
 *******************************************************************************/
 
+// Factorial: O(n)
 unsigned fact(unsigned n) {
-    // base cases (combined)
-    if (n <= 1) {
-        return 1;
-    }
-
-    // recursive case
-    unsigned res = fact(n - 1);
-    return res * n;
+    return factHelper(n, 1);
 }
 
+unsigned factHelper(unsigned n, unsigned acc) {
+    if (n <= 1)
+        return acc;
+    return factHelper(n - 1, acc * n);
+}
+
+
+// Fibonacci: O(n) instead of O(2^n)
 unsigned fib(unsigned n) {
-    // base case 1
-    if (n == 0) {
-        return 0;
-    }
-
-    // base case 2
-    else if (n == 1) {
-        return 1;
-    }
-
-    // recursive case
-    return fib(n - 1) + fib(n - 2);
+    return fibHelper(n, 0, 1);
 }
 
+unsigned fibHelper(unsigned n, unsigned a, unsigned b) {
+    if (n == 0)
+        return a;
+    return fibHelper(n - 1, b, a + b);
+}
+
+
+// Multiplication via repeated addition: O(y)
 unsigned mult(unsigned x, unsigned y) {
-    // base case
-    if (y == 0) {
-        return 0;
-    }
-
-    // recursive case
-    unsigned res = mult(x, y - 1);
-    return res + x;
+    return multHelper(x, y, 0);
 }
 
+unsigned multHelper(unsigned x, unsigned y, unsigned acc) {
+    if (y == 0)
+        return acc;
+    return multHelper(x, y - 1, acc + x);
+}
+
+
+// Power function: O(y)
 unsigned power(unsigned x, unsigned y) {
-    // base case
-    if (y == 0) {
-        return 1;
-    }
-
-    // recursive case
-    unsigned res = power(x, y - 1);
-    return res * x;
+    return powerHelper(x, y, 1);
 }
 
-unsigned product(unsigned x, unsigned y) {
-    // base case
-    if (x == y) {
-        return x;
-    }
+unsigned powerHelper(unsigned x, unsigned y, unsigned acc) {
+    if (y == 0)
+        return acc;
+    return powerHelper(x, y - 1, acc * x);
+}
 
-    // recursive case
-    unsigned p = product(x + 1, y);
-    return p * x;
+
+// Product from x to y: O(y - x)
+unsigned product(unsigned x, unsigned y) {
+    return productHelper(x, y, 1);
+}
+
+unsigned productHelper(unsigned x, unsigned y, unsigned acc) {
+    if (x > y)
+        return acc;
+    return productHelper(x + 1, y, acc * x);
 }
